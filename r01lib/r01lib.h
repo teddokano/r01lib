7745cc4c5f9c8918ab57c05d4b57c827b3d67f62
Tedd OKANO <okano.akifumi@gmail.com>
2025-07-06 15:11:37 +0900

    Semihost/UART debug console switching compatibility fix at MCUXpresso v25.6

--- r01lib/r01lib.h
+++ r01lib/r01lib.h
@@ -11,11 +11,16 @@ extern "C" {
 #include	"fsl_debug_console.h"
 }
 
+#define		MCUXPRESSO_V25_06_OR_LATER 
+
 #if (defined(SDK_DEBUGCONSOLE) && (SDK_DEBUGCONSOLE == DEBUGCONSOLE_REDIRECT_TO_SDK))
+#ifdef	MCUXPRESSO_V25_06_OR_LATER
+#else	//	MCUXPRESSO_V25_06_OR_LATER
 #define printf	DbgConsole_Printf
 #define scanf	DbgConsole_Scanf
 #define putchar	DbgConsole_Putchar
 #define getchar	DbgConsole_Getchar
+#endif	//	MCUXPRESSO_V25_06_OR_LATER
 #else
 #define		SEMIHOST_OPERATION
 #endif
@@ -29,7 +34,10 @@ extern "C" {
 #include	"BusInOut.h"
 #include	"mcu.h"
 
+#ifdef	MCUXPRESSO_V25_06_OR_LATER
+#else	//	MCUXPRESSO_V25_06_OR_LATER
 #include	<iostream>
 #include	<iomanip>
+#endif	//	MCUXPRESSO_V25_06_OR_LATER
 
 #endif // R01LIB_R01LIB_H
